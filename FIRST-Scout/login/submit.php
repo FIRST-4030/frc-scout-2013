5d81c4127d9ec35a7aadc12ce2a3ae0928e352a2
DrCoffey <DrCoffey@192.168.2.4>
2013-03-01 00:57:02 -0800

    initial commit

---
+++ FIRST-Scout/login/submit.php
@@ -0,0 +1,10 @@
+<?php
+    $teamNum = $_POST['team_number']; 
+    $userEmail = $_POST['user_email'];
+    $userPass = $_POST['user_password'];
+    
+    
+    echo 'team number: ' . $teamNum . '<br>';
+    echo 'user email: ' . $userEmail . '<br>';
+    echo 'user password: ' . $userPass . '<br>';
+?>
