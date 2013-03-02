a7fe5320da34396afb006e3ecb6ce13d7362d423
Sam <Sam@192.168.1.24>
2013-03-01 21:15:35 -0800

    Updated login page and caught some errors. Also optimized for mobile.

--- FIRST-Scout/index.php
+++ FIRST-Scout/index.php
@@ -5,21 +5,21 @@
         <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
         <link href="css/style.css" rel="stylesheet" type="text/css">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
     </head>
     <body>
         <div class="container">
-            <div class="row">
-                <div class="twelvecol">
-                    <? echo stripcslashes($_GET['error']); ?>
-                    <p class="title">FIRST Scout: Login</p>
-                    <form method="post" action="submit.php">
-                        <input type="text" name="team_number" placeholder="Team Number"><br> 
-                        <input type="email" name="user_email" placeholder="Email Address"><br>
-                        <input type="password" name="user_password" placeholder="Password"><br>
-                        <input type="submit" value="Log In" style="margin-top: 5px">
-                    </form>
-                </div>
-            </div>
+            <a href="/index.php"><img src="images/ram-logo.png" alt="FIRST Scout"></a>
+            <br>
+            <? echo stripcslashes($_GET['error']); ?>
+            <p class="title">FIRST Scout: Login</p>
+            <form class="form_entry" method="post" action="submit.php">
+                <input type="text" name="team_number" placeholder="Team Number"><br> 
+                <input type="email" name="user_email" placeholder="Email Address"><br>
+                <input type="password" name="user_password" placeholder="Password"><br>
+                <input type="submit" value="Log In" style="margin-top: 5px">
+            </form>
+
         </div>
     </body>
 </html>
