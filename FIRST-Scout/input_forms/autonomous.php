032ac4ed5af7fe915a2e908af89f1364c14341af
Sam <>
2013-03-08 12:44:09 -0800

    Temporary test stuff

--- FIRST-Scout/input_forms/autonomous.php
+++ FIRST-Scout/input_forms/autonomous.php
@@ -16,22 +16,20 @@
         <script type="text/javascript" src="../jqwidgets/jqwidgets/jqxinput.js"></script>
         <script type="test/javascript" src="../includes/Cookies.js"></script>
         <script type="text/javascript" src="../jqwidgets/jqwidgets/jqxcheckbox.js"></script>
+        <script type="text/javascript" src="../functions/Cookies.js"></script>
         <link href='../css/custom.css' rel="stylesheet" type="text/css">
 
     </head>
     <body>
         <script type="text/javascript">
-            /*
-             * TODO: add individual point variables for each score of different
-             * points and subtract them dynamically
-             */
+
             $(document).ready(function() {
                 window.scrollTo(0, 1);
 
                 var autoPointsScored6 = 0;
                 var autoPointsScored4 = 0;
                 var autoPointsScored2 = 0;
-
+                var autonomousPoints = new Array();
 
                 $("#SixPointPlus").jqxRepeatButton({delay: 0, width: '100', height: '50', theme: 'custom'});
                 $("#SixPointMinus").jqxRepeatButton({delay: 0, width: '50', height: '50', theme: 'custom'});
@@ -40,13 +38,19 @@
                 $("#TwoPointPlus").jqxRepeatButton({delay: 0, width: '100', height: '50', theme: 'custom'});
                 $("#TwoPointMinus").jqxRepeatButton({delay: 0, width: '50', height: '50', theme: 'custom'});
                 $("#NextPageButton").jqxButton({width: '252px', height: '50px', theme: 'custom'});
-                $("#usedKinect").jqxCheckBox({width: 100, height: 30, theme: 'theme'});
+                $("#usedKinect").jqxCheckBox({width: 150, height: 30, theme: 'theme'});
 
                 $("#SixPointPlus").on("click", function() {
                     autoPointsScored6++;
                     updateTotals(autoPointsScored6, autoPointsScored4, autoPointsScored2);
                     updateIndividualTotals();
                 });
+          
+          
+                function onClick(buttonID) {
+                    
+                }
+                        
                 $("#SixPointMinus").on("click", function() {
                     if (autoPointsScored6 > 0) {
                         autoPointsScored6--;
@@ -95,6 +99,10 @@
             function updateTotals(ps6, ps4, ps2) {
                 document.getElementById('totalPoints').innerHTML = (ps6 * 6) + (ps4 * 4) + (ps2 * 2);
             }
+            
+            function update(i, j) {
+                document.getElementById('id').innerHTML = i;
+            }
 
         </script>
 
@@ -102,24 +110,25 @@
             <p class="title">Autonomous Mode</p>
             <div id="usedKinect" style="margin-left: auto; margin-right: auto; margin-top: 5px; margin-bottom: 5px; font-family: Ubuntu, Arial, sans-serif; font-size: 18px">Used Kinect</div>
             <p><i>Record points for each goal</i></p>
-            <input type="button" id="SixPointPlus" value="+6" class="input_forms" />
-            <input type="button" id="SixPointMinus" value="&mdash;" class="input_forms" /> 
+            <input type="button" onclick="update('autoSixPoints', false)" id="SixPointPlus" value="+6" class="input_forms" />
+            <input type="button" onclick="update('autoSixPoints', true)" id="SixPointMinus" value="&mdash;" class="input_forms" /> 
             <span id="autoSixPoint" class="autonomousIndividual">0</span>
             <br><br>
-            <input type="button" id="FourPointPlus" value="+4" class="input_forms" />
-            <input type="button" id="FourPointMinus" value="&mdash;" class="input_forms" /> 
+            <input type="button" onclick="update('autoFourPoints', false)" id="FourPointPlus" value="+4" class="input_forms" />
+            <input type="button" onclick="update('autoFourPoints', true)" id="FourPointMinus" value="&mdash;" class="input_forms" /> 
             <span id="autoFourPoint" class="autonomousIndividual">0</span>
 
             <br><br>
-            <input type="button" id="TwoPointPlus" value="+2" class="input_forms" />
-            <input type="button" id="TwoPointMinus" value="&mdash;" class="input_forms" /> 
+            <input type="button" onclick="update('autoTwoPoints', false)" id="TwoPointPlus" value="+2" class="input_forms" />
+            <input type="button" onclick="update('autoTwoPoints', true)" id="TwoPointMinus" value="&mdash;" class="input_forms" /> 
             <span id="autoTwoPoint" class="autonomousIndividual">0</span>
 
             <br>
             <p style="font-weight: bold">Total Points: <span id="totalPoints"></span></p>
-            
+
             <input type="button" class="centered" id="NextPageButton" value="Continue to Teleoperated &rarr;">
             <br>
+            <p id="id" />
         </div>
     </body>
 </html>
