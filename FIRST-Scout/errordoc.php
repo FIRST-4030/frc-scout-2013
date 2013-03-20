820311b8b447253fbe9c871855f68aff97ff809a
Sam <>
2013-03-20 12:43:16 -0700

    Added error pages.

---
+++ FIRST-Scout/errordoc.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        $error = $_GET['error'];
+        $errorMessage = "";
+        switch ($error) {
+            case 400:
+                $errorMessage = "Error 400: Bad Request.";
+                break;
+            case 401:
+                $errorMessage = "Error 401: Authorization required.";
+                break;
+            case 403:
+                $errorMessage = "Error 403: Forbidden.";
+                break;
+            case 404:
+                $errorMessage = "Error 404: Page not found.";
+                break;
+            case 500:
+                $errorMessage = "Error 500: Internal server error.";
+                break;
+        }
+        if($errorMessage === "") {
+            $errorMessage = "Error $error: Unknown error.";
+        }
+        ?>
+        <title>Error <? echo $error ?></title>
+        <?php include 'includes/form-headers.html'; ?>
+    </head>
+    <body>
+        <div class='container'>
+            <img src="/images/ram-logo.png" alt="FIRST Scout">
+            <div class="alert alert-warning" id="inputError">
+                <strong id='alertError'><?php echo $errorMessage ?></strong>
+            </div>
+            <p class="title">Aw, snap! You've encountered an error!</p>
+            <p class='small_title'>What can you do now?</p>
+            <br />
+            <button class='btn btn-large btn-info' onclick='window.location = "options"'>Return Home</button>
+            <br /> <br /> 
+        </div>
+    </body>
+</html>
