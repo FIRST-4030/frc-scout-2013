22767379063b1528bcd3718c0c5a3baeb2994f51
ing_sswolfson <ing_sswolfson@WINGRM20014.seattleschools.org>
2013-03-20 13:20:20 -0700

    Added error submit.

--- FIRST-Scout/errordoc.php
+++ FIRST-Scout/errordoc.php
@@ -21,11 +21,16 @@
                 $errorMessage = "Error 500: Internal server error.";
                 break;
         }
-        if($errorMessage === "") {
-            $errorMessage = "Error $error: Unknown error.";
+        if (isset($_POST['error_submit'])) {
+            $userMessage = $_POST['error_submit'];
+            mail("terabyte128@gmail.com", "Error $error was encountered on FIRST Scout", "A user encountered error $error and left this message:\r\n\r\n$userMessage");
+            $errorMessage = "Error reported successfully, thanks!";
+        }
+        if ($errorMessage === "") {
+            $errorMessage = "Error $error: Unknown error, what did you do?";
         }
         ?>
-        <title>Error <? echo $error ?></title>
+        <title>Error <? echo $errorMessage ?></title>
         <?php include 'includes/form-headers.html'; ?>
     </head>
     <body>
@@ -40,6 +45,26 @@
             <br />
             <button class='btn btn-large btn-success homepage_buttons' onclick='window.location = "index.php"'>Login</button>
             <br />
+            <button class='btn btn-large btn-danger homepage_buttons' onclick='showError()'>Report this error</button>
+            <form action="/errordoc.php?error=<? echo $error ?>" id="errorForm" method="post">
+                <textarea name="error_submit" placeholder="Enter any information." style="width: 190px; height: 100px"></textarea>
+                <br />
+                <button type="submit" class="btn btn-success">Submit</button>
+            </form>
         </div>
+        <script type="text/javascript">
+                $(document).ready(function() {
+                    $("#errorForm").hide();
+                });
+
+                function showError() {
+                    var errorForm = $("#errorForm");
+                    if (errorForm.is(":visible")) {
+                        errorForm.hide();
+                    } else {
+                        errorForm.show();
+                    }
+                }
+        </script>
     </body>
 </html>
