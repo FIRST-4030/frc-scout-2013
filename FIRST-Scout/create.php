76c2e9e9bf2eabbd60ed457bcdceee703b5188ac
Sam <Sam@192.168.1.164>
2013-03-24 17:31:29 -0700

    .

--- FIRST-Scout/create.php
+++ FIRST-Scout/create.php
@@ -6,29 +6,49 @@ if (isset($_POST['team_id'])) {
     $teamPassword = $_POST['team_password'];
     $teamNumber = $_POST['team_number'];
     $adminEmail = $_POST['team_email'];
+    $errorMessage = "Please correct the following errors:<br />";
+    if(empty($teamID)) {
+        $errorMessage .= "&bull; Enter a Team ID.<br />";
+    } 
+    if(empty($teamPassword)) {
+        $errorMessage .= "&bull; Enter a team password.<br />";
+    }
+    if(empty($teamNumber)) {
+        $errorMessage .= "&bull; Enter a team number.<br />";
+    }
+    if(empty($teamPassword)) {
+        $errorMessage .= "&bull; Enter an admin email.<br />";
+    }
+    
+    if(substr_count($errorMessage, "&bull;") > 0) {
+        header('location: ?error=' . urlencode($errorMessage));
+        exit();
+    }
+    
     # Connect to the DB
+    echo $teamID . $teamPassword . $teamNumber . $adminEmail;
     try {
         $db = new PDO(DSN, DB_USER, DB_PASSWD);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $ex) {
         die("Unable to connect to DB\n");
     }
-    
-    $retrieve = $db->prepare("SELECT uid FROM scout_login WHERE team_id=?");
+
+    $retrieve = $db->prepare("SELECT team_id FROM scout_login WHERE team_id=?");
     $retrieve->execute(array($teamID));
     $otherAccouts = $retrieve->fetch(PDO::FETCH_ASSOC);
-    if(!empty($otherAccouts)) {
+    if (key_exists($teamID, $otherAccouts)) {
         header('location: ?error=' . urlencode("That Team ID already exists!"));
     }
-    
+
     try {
-        $create = $db->prepare("INSERT INTO scout_login (team_id, team_password, team_number, team_admin_email) VALUES (?, ms5(?), ?, ?)");
+        $create = $db->prepare("INSERT INTO scout_login (team_id, team_password, team_number, team_admin_email) VALUES (?, md5(?), ?, ?)");
         $create->execute(array($teamID, $teamPassword, $teamNumber, $adminEmail));
         //$db->commit();
     } catch (PDOException $e) {
         die("Couldn't save data: " . $e->getMessage());
     }
-    
+
     mail($adminEmail, "FIRST Scout account created!", "You have successfully created an account with FIRST Scout.\r\n\r\nTeam ID: $teamID\r\nTeam Password: $teamPassword\r\nTeam Number: $teamNumber\r\nAdmin Email $adminEmail", "From: 'Scout Bot' <scout@ingrahamrobotics.org");
     header('location: index.php?error=' . urlencode("Account creation successful! Please login now."));
 }
@@ -49,6 +69,10 @@ if (isset($_POST['team_id'])) {
                 scouted what match.<br /><br/> 
                 The account's <b>admin email</b> should be the email address of whoever is head of scouting for your team. It would only be used in case we need to contact you about something, it is never shared.
                 <br /><br /></p>
+            <div class="alert alert-warning" id="inputError">
+                <button type="button" class="close" onclick="$('.alert').hide()">&times;</button>
+                <strong id='alertError'><?php if (isset($_GET['error'])) echo stripcslashes($_GET['error']); ?></strong>
+            </div>
             <p><strong>Ready to get started?</strong></p>
             <form method="post" action="create.php">
                 Team ID:<br /><input type="text" name="team_id" placeholder="i.e. NullPointerException" /><br />
@@ -58,5 +82,14 @@ if (isset($_POST['team_id'])) {
                 <button class="btn btn-success" type="submit">Submit</button>
             </form>
         </div>
+        <script type="text/javascript">
+                    $(document).ready(function() {
+                        $('#inputError').hide();
+
+                        if (document.getElementById('alertError').innerHTML !== "") {
+                            $('#inputError').show();
+                        }
+                    });
+        </script>
     </body>
 </html>
