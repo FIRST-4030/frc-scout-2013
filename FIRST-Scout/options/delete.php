f2d4919478d3670f0d058e3e02728c6b043be557
Sam <Sam@192.168.1.164>
2013-03-28 12:45:54 -0700

    Code formatting!

--- FIRST-Scout/options/delete.php
+++ FIRST-Scout/options/delete.php
@@ -5,13 +5,15 @@ session_start();
 if (!isset($_POST['id'])) {
     header('location: index.php?error=' . urlencode("YOU CANNOT DO THAT!"));
 }
+
 $uid = intval($_POST['id']);
 
 if (!isset($_SESSION['TeamID'])) {
     header('location: ../index.php?error=' . urlencode("No."));
 }
 
-require 'includes/constants.php';
+require '../includes/constants.php';
+
 try {
     $db = new PDO(DSN, DB_USER, DB_PASSWD);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -21,9 +23,9 @@ try {
 
 try {
 	$del = $db->prepare('DELETE FROM `scout_recording` WHERE uid = ? AND team_id = ?');
-	$authenticate->execute(array($uid, $_SESSION['TeamID']));
+	$del->execute(array($uid, $_SESSION['TeamID']));
 } catch (PDOException $ex) {
-    die("Unable to delete\n " . $ex->getMessage());
+    echo "Unable to delete\n " . $ex->getMessage();
 }
 
 echo "Deleted successfully";
