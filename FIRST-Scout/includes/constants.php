3624152a04496fa06f2a6146c6bbc9738ec2af71
Sam <Sam@192.168.1.24>
2013-04-02 22:24:08 -0700

    No constants for you!

--- FIRST-Scout/includes/constants.php
+++ FIRST-Scout/includes/constants.php
@@ -1,11 +1,11 @@
 <?
-define("DB_NAME", "stevenz9_robotics_scout");
-define("LOGIN_TABLE", "scout_login");
-define("MATCH_TABLE", "scout_recording");
-define("LOCATION_TABLE", "stevenz9_robotics_locations");
-define("DSN", "mysql:host=localhost;dbname=stevenz9_robotics_scout;charset=utf8");
-define("DB_USER", "stevenz9_user");
-define("DB_PASSWD", "=VO^wTT6_I++");
+define("DB_NAME", "");
+define("LOGIN_TABLE", "");
+define("MATCH_TABLE", "");
+define("LOCATION_TABLE", "");
+define("DSN", "");
+define("DB_USER", "");
+define("DB_PASSWD", "");
 define("COLOR_RED", "rgb(241, 138, 138)");
 define("COLOR_BLUE", "rgb(60, 91, 255)");
 ?>
